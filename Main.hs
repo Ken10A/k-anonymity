b6d57cbcd2e27e4d105a3f1a6427d32ac7d4d0de
KENN <ken@nac.local>
2017-03-20 09:12:38 +0900

    main was so long
    move codes to util

--- Main.hs
+++ Main.hs
@@ -1,29 +1,7 @@
 module Main(main) where
 
-import Data.List
-import Data.List.Split
+import System.Environment
 import Util
 
-
-main = do
-
-  -- csv読み込み
-  f <- readFile "./Data/dataset100.txt"
-  let dataset = map (\dataset -> splitOn "," dataset) $ lines f
-  -- putStrLn "getting dataset done..."
-  -- print dataset
-
-  -- 1データセットから作成できる全ての匿名段階の匿名化データセット作成
-  let all_anonymous_degrees =
-        makeAllAnonymousDegreesCombination $ getRecordElemLength $ dataset !! 0
-  let processed =
-        map (\degrees-> anonymizeDataset dataset degrees) all_anonymous_degrees
-  putStrLn "anonymization done..."
-  print processed
-
-  
-
-  -- リストの
-  
-
-  -- 
+main :: IO()
+main = k_anonymize =<< getArgs
